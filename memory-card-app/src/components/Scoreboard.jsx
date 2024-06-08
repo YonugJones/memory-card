bcc751c738783cb486a77d89ca0fc1914dfa6461
YonugJones <peterckerfoot@gmail.com>
2024-06-08 14:41:12 -0500

    added logic for Scoreboard component

---
+++ memory-card-app/src/components/Scoreboard.jsx
@@ -0,0 +1,10 @@
+import '../styles/Scoreboard'
+
+export default function Scoreboard({ score, bestScore }) {
+  return (
+    <div className='scoreboard'>
+      <div>Score: {score}</div>
+      <div>Best Score: {bestScore}</div>
+    </div>
+  )
+}
