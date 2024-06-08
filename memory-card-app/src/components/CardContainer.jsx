d0fbce0d410641bf913850e97d19e08396f9b06e
YonugJones <peterckerfoot@gmail.com>
2024-06-08 14:50:36 -0500

    added logic for CardContainer component with images and onCardClick props

--- memory-card-app/src/components/CardContainer.jsx
+++ memory-card-app/src/components/CardContainer.jsx
@@ -1,2 +1,12 @@
 import '../styles/CardContainer'
+import Card from './Card'
 
+export default function CardContainer({ images, onCardClick }) {
+  return (
+    <div className='cardContainer'>
+      {images.map(image => (
+        <Card key={image.id} image={image} onClick={onCardClick} />
+      ))}
+    </div>
+  )
+}
